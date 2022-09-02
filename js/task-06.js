5df44d51fd581f48ab43661a28d869d5df04c038
Килимник Дмитро <dinakilimnik@gmail.com>
2022-09-02 12:34:13 +0300

    ?

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,15 @@
 const inputEL = document.querySelector('input');
+
+
+inputEL.addEventListener('blur', funk);
+const atrebuts = inputEL.getAttribute('data-length');
+const lengthInput = inputEL.value.length;
+
+function funk(event){
+    if(atrebuts !== lengthInput){
+        inputEL.classList.add('invalid');
+
+    }else{
+        inputEL.classList.add('valid');
+    }
+}
