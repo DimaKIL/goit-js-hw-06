b0a01eac57a1f2db607f6f328e6bb8d353137b76
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-04 11:07:31 +0300

    прпр

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let arr = [];
 const listEL = document.querySelector('#ingredients');
 for(const ingredient of ingredients){
   const itemEl = document.createElement('li');
   itemEl.textContent = ingredient;
   itemEl.classList.add('item');
   listEL.appendChild(itemEl);
+
+
 }
 console.log(listEL);
